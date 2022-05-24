6c42005e36dd2b25ea96fa2c05edb937849c455e
Joshua McMiller <jjmcmi113r@gmail.com>
2022-05-24 15:22:28 -0500

    updated server get and create routes

--- public/app.js
+++ public/app.js
@@ -1,13 +1,92 @@
-window.addEventListener('load', () => {
-	// getAllTasks()
-	const form = document.querySelector("#new-task-form");
-	const input = document.querySelector("#new-task-input");
-	const list_el = document.querySelector("#tasks");
+const form = document.querySelector("#new-task-form");
+const input = document.querySelector("#new-task-input");
+const list_el = document.querySelector("#tasks");
+
+
+window.addEventListener('load', getAllTasks)
+
 
 	form.addEventListener('submit', (e) => {
-		e.preventDefault();
+		createTask(e);
 
-		const task = input.value;
+		// const task = input.value;
+
+		// const task_el = document.createElement('div');
+		// task_el.classList.add('task');
+
+		// const task_content_el = document.createElement('div');
+		// task_content_el.classList.add('content');
+
+		// task_el.appendChild(task_content_el);
+
+		// const task_input_el = document.createElement('input');
+		// task_input_el.classList.add('text');
+		// task_input_el.type = 'text';
+		// task_input_el.value = task;
+		// task_input_el.setAttribute('readonly', 'readonly');
+
+		// task_content_el.appendChild(task_input_el);
+
+		// const task_actions_el = document.createElement('div');
+		// task_actions_el.classList.add('actions');
+		
+		// const task_edit_el = document.createElement('button');
+		// task_edit_el.classList.add('edit');
+		// task_edit_el.innerText = 'Edit';
+
+		// const task_delete_el = document.createElement('button');
+		// task_delete_el.classList.add('delete');
+		// task_delete_el.innerText = 'Delete';
+
+		// task_actions_el.appendChild(task_edit_el);
+		// task_actions_el.appendChild(task_delete_el);
+
+		// task_el.appendChild(task_actions_el);
+
+		// list_el.appendChild(task_el);
+
+		// input.value = '';
+
+		// task_edit_el.addEventListener('click', (e) => {
+		// 	if (task_edit_el.innerText.toLowerCase() == "edit") {
+		// 		task_edit_el.innerText = "Save";
+		// 		task_input_el.removeAttribute("readonly");
+		// 		task_input_el.focus();
+		// 	} else {
+		// 		task_edit_el.innerText = "Edit";
+		// 		task_input_el.setAttribute("readonly", "readonly");
+		// 	}
+		// });
+
+		// task_delete_el.addEventListener('click', (e) => {
+		// 	list_el.removeChild(task_el);
+		// });
+	});
+
+
+function createTask(e) {
+	e.preventDefault()
+	const task = input.value;
+	const date = Date();
+	const jsonDate = (new Date()).toJSON()
+	taskData = {
+		task_content: `${task}`,
+		due_date: jsonDate,
+		completed: "FALSE"
+	};
+	console.log(taskData)
+	
+	$.post( "https://jjmac7777-mvp1.herokuapp.com/api/create", taskData, function( data ) {
+		$( ".result" ).html( data );
+	  });
+}
+
+
+function getAllTasks() {
+  
+	$.get("https://jjmac7777-mvp1.herokuapp.com/api/tasks" , (data) => {
+		for (let i = 0; i < data.length; i++) {
+			const task = data[i].task_content;
 
 		const task_el = document.createElement('div');
 		task_el.classList.add('task');
@@ -43,8 +122,6 @@ window.addEventListener('load', () => {
 
 		list_el.appendChild(task_el);
 
-		input.value = '';
-
 		task_edit_el.addEventListener('click', (e) => {
 			if (task_edit_el.innerText.toLowerCase() == "edit") {
 				task_edit_el.innerText = "Save";
@@ -59,16 +136,12 @@ window.addEventListener('load', () => {
 		task_delete_el.addEventListener('click', (e) => {
 			list_el.removeChild(task_el);
 		});
-	});
-
-});
-
-function getAllTasks() {
-  
-	$.get("https://jjmac7777-mvp1.herokuapp.com/api/tasks" , (data) => {
-		
+		}
 		console.log(data)
 		
   })
 }
-getAllTasks()
+// getAllTasks()
+
+ //POST JSON data to the API
+//  $.post( "youdomain.com/message", JSON.stringify(yourDataObjectOrArray));
--- server.js
+++ server.js
@@ -2,11 +2,11 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const db = require("./db/conn")
-// const cors = require("cors")
+const cors = require("cors")
 
-// app.use(cors({
-//     origin: "http://10.174.102.220:5500"
-// }))
+app.use(cors({
+    origin: "http://127.0.0.1:5500"
+}))
 
 app.use(express.json())
 app.use(express.static("public"));
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 app.get("/api/tasks", async (_, res) => {
     try {
         await db.query('SELECT * FROM tasks', (error, results) => {
-            console.log(results)
+            // console.log(results)
             res.status(200).json(results.rows)
         })
     } catch (error) {
@@ -22,6 +22,23 @@ app.get("/api/tasks", async (_, res) => {
     }
   });
 
+app.post("/api/create", async (req, res) => {
+   try {
+    const {task_content, due_date, completed} = req.body
+    await db.query('INSERT INTO tasks (task_content, due_date, completed) VALUES ($1, $2, $3)', [task_content, due_date, completed], (error, results) => {
+        console.log(req.body)
+        res.status(200).send(`${req.body} task was added`)
+    })
+    } catch (error) {
+       console.error(error.message)
+    }
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port: ${process.env.PORT}`)
+})
+
+//Error handling
+app.use((req, res) => {
+    res.status(404).send("Not Found")
 })
