2d985eddd9112ee3ac78f61caed68b9eb09ffeaf
Joshua McMiller <jjmcmi113r@gmail.com>
2022-05-24 12:34:18 -0500

    updating repo

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', () => {
+	// getAllTasks()
 	const form = document.querySelector("#new-task-form");
 	const input = document.querySelector("#new-task-input");
 	const list_el = document.querySelector("#tasks");
@@ -62,11 +63,12 @@ window.addEventListener('load', () => {
 
 });
 
-function getTasks() {
+function getAllTasks() {
   
-	$.get("" , (data) => {
+	$.get("https://jjmac7777-mvp1.herokuapp.com/api/tasks" , (data) => {
 		
 		console.log(data)
 		
   })
 }
+getAllTasks()
